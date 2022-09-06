a6a5ebe541802752c457f0a31c6afc7533030625
quinnrong <quinnrong@tencent.com>
2022-09-06 11:10:09 +0800

    update

--- gflops-cuda/build.sh
+++ gflops-cuda/build.sh
@@ -4,13 +4,13 @@ set -ex
 
 FP16=${1:-0}
 if [ $FP16 == 0 ]; then
-    nvcc                                            \
+    nvcc -std=c++11                                 \
         --generate-code arch=compute_75,code=sm_75  \
         bench_fp32.cu                               \
         -o bench_fp32
     ./bench_fp32
 else
-    nvcc                                            \
+    nvcc -std=c++11                                 \
         --generate-code arch=compute_75,code=sm_75  \
         bench_fp16.cu                               \
         -o bench_fp16
